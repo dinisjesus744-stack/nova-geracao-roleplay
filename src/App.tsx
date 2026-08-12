7010d501e6bddcfca2117389bcc8667408741598
Dinis Jesus <dinisjesus744@gmail.com>
2026-08-12 23:49:43 +0100

    Fix blank page issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
-// Stats, News and Store removed per user request
 import HowTo from './components/HowTo'
 import Exclusives from './components/Exclusives'
 import Highlight from './components/Highlight'
-import News from './components/News'
 import Discord from './components/Discord'
-import Store from './components/Store'
 import Footer from './components/Footer'
 
 export default function App() {
